0c434ef97f23e99a47ca5b1ce7f1eaeab13fdc40
Manfred2023 <moukatemanfred@gmail.com>
2025-11-25 23:52:00 +0100

    add tailwind CSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <header class="min-h-screen bg-primary">
+    <div class="flex items-center justify-between text-onSecondary p-5">
+      <div class="text-2xl font-bold"><h2>FredCode</h2></div>
+      <div class="flex items-center justify-between gap-6">
+        <h2>Home</h2>
+        <h2>Service</h2>
+        <h2>Patner</h2>
+        <h2>About Us</h2>
+      </div>
+      <div><h2>Contact Us</h2></div>
+    </div>
+  </header>
 </template>
